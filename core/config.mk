ef19167dff5c90ba973373e1234922fd1cfdea5b
Semavi Ulusoy <doc.divxm@gmail.com>
2022-08-30 21:33:02 +0300

    build: v15.0: AICPfy
    Change-Id: I6e76e474aeb608bbcb5da9cef2eb8f5d792988c3

--- core/config.mk
+++ core/config.mk
@@ -458,8 +458,8 @@ endif
 # See envsetup.mk for a description of SCAN_EXCLUDE_DIRS
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
-ifneq ($(LINEAGE_BUILD),)
-include vendor/lineage/config/BoardConfigLineage.mk
+ifneq ($(AICP_BUILD),)
+include vendor/aicp/config/BoardConfigAicp.mk
 endif
 
 # The build system exposes several variables for where to find the kernel
@@ -1307,11 +1307,11 @@ include $(BUILD_SYSTEM)/sysprop_config.mk
 # consistency with those defined in BoardConfig.mk files.
 include $(BUILD_SYSTEM)/android_soong_config_vars.mk
 
-ifneq ($(LINEAGE_BUILD),)
-ifneq ($(wildcard device/lineage/sepolicy/common/sepolicy.mk),)
+ifneq ($(AICP_BUILD),)
+ifneq ($(wildcard device/aicp/sepolicy/common/sepolicy.mk),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include device/lineage/sepolicy/common/sepolicy.mk)
+$(eval include device/aicp/sepolicy/common/sepolicy.mk)
 endif
 endif
 
