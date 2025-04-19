e4543f8425a55fd7f3efa89c72991ebc4424c000
George Zacharia <george.zcharia@gmail.com>
2025-04-19 08:36:24 +0530

    fixup! build: switch back to lineage sepolicy and add include aicp sepolicy too
    Change-Id: If96a95c284407157a2d3e69c471f648041b14db9

--- core/config.mk
+++ core/config.mk
@@ -1308,10 +1308,10 @@ include $(BUILD_SYSTEM)/sysprop_config.mk
 include $(BUILD_SYSTEM)/android_soong_config_vars.mk
 
 ifneq ($(AICP_BUILD),)
-ifneq ($(wildcard device/aicp/sepolicy/common/sepolicy.mk),)
+ifneq ($(wildcard device/lineage/sepolicy/common/sepolicy.mk),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include device/aicp/sepolicy/common/sepolicy.mk)
+$(eval include device/lineage/sepolicy/common/sepolicy.mk)
 endif
 ifneq ($(wildcard device/aicp/sepolicy/common/sepolicy.mk),)
 $(eval include device/aicp/sepolicy/common/sepolicy.mk)
